b859688eb589a77c66afd50fa0878377b2221528
Giuseppe Capnetto <ggcaponetto@gmail.com>
2023-04-26 21:51:22 +0200

    wip fn list

--- src/extensions/chrome-extension-boilerplate-react-vite-main/src/pages/popup/components/fn-store/FnStore.tsx
+++ src/extensions/chrome-extension-boilerplate-react-vite-main/src/pages/popup/components/fn-store/FnStore.tsx
@@ -15,6 +15,9 @@ import Paper from "@mui/material/Paper";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 import "./FnStore.css";
+import DeleteIcon from "@mui/icons-material/Delete";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+
 const ll = log.getLogger("FnStore.tsx");
 
 import process from "process";
@@ -28,31 +31,15 @@ if (process.env.VITE_ENV === "development" && isLogsEnabled) {
 
 function createData(
   name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
-  price: number
+  comment: number,
+  status: number,
+  order: number
 ) {
   return {
     name,
-    calories,
-    fat,
-    carbs,
-    protein,
-    price,
-    history: [
-      {
-        date: "2020-01-05",
-        customerId: "11091700",
-        amount: 3,
-      },
-      {
-        date: "2020-01-02",
-        customerId: "Anonymous",
-        amount: 1,
-      },
-    ],
+    comment,
+    status,
+    order,
   };
 }
 
@@ -75,43 +62,19 @@ function Row(props: { row: ReturnType<typeof createData> }) {
         <TableCell component="th" scope="row">
           {row.name}
         </TableCell>
-        <TableCell align="right">{row.calories}</TableCell>
-        <TableCell align="right">{row.fat}</TableCell>
-        <TableCell align="right">{row.carbs}</TableCell>
-        <TableCell align="right">{row.protein}</TableCell>
+        <TableCell align="right">{row.comment}</TableCell>
+        <TableCell align="right">{row.status}</TableCell>
+        <TableCell align="right">{row.order}</TableCell>
+        <TableCell align="right">
+          <IconButton>
+            <DeleteIcon></DeleteIcon>
+          </IconButton>
+        </TableCell>
       </TableRow>
       <TableRow>
         <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
           <Collapse in={open} timeout="auto" unmountOnExit>
-            <Box sx={{ margin: 1 }}>
-              <Typography variant="h6" gutterBottom component="div">
-                History
-              </Typography>
-              <Table size="small" aria-label="purchases">
-                <TableHead>
-                  <TableRow>
-                    <TableCell>Date</TableCell>
-                    <TableCell>Customer</TableCell>
-                    <TableCell align="right">Amount</TableCell>
-                    <TableCell align="right">Total price ($)</TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {row.history.map((historyRow) => (
-                    <TableRow key={historyRow.date}>
-                      <TableCell component="th" scope="row">
-                        {historyRow.date}
-                      </TableCell>
-                      <TableCell>{historyRow.customerId}</TableCell>
-                      <TableCell align="right">{historyRow.amount}</TableCell>
-                      <TableCell align="right">
-                        {Math.round(historyRow.amount * row.price * 100) / 100}
-                      </TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
-            </Box>
+            Some collapsable content
           </Collapse>
         </TableCell>
       </TableRow>
@@ -119,34 +82,22 @@ function Row(props: { row: ReturnType<typeof createData> }) {
   );
 }
 
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0, 3.99),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3, 4.99),
-  createData("Eclair", 262, 16.0, 24, 6.0, 3.79),
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0, 3.99),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3, 4.99),
-  createData("Eclair", 262, 16.0, 24, 6.0, 3.79),
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0, 3.99),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3, 4.99),
-  createData("Eclair", 262, 16.0, 24, 6.0, 3.79),
-];
-
-export function CollapsibleTable() {
+export function CollapsibleTable(props) {
   return (
     <TableContainer component={Paper}>
       <Table aria-label="collapsible table">
         <TableHead>
           <TableRow>
             <TableCell />
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Name</TableCell>
+            <TableCell align="right">Comment</TableCell>
+            <TableCell align="right">Status</TableCell>
+            <TableCell align="right">Order</TableCell>
+            <TableCell align="right">Actions</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
+          {props.excelFunctions.map((row) => (
             <Row key={row.name} row={row} />
           ))}
         </TableBody>
@@ -155,13 +106,46 @@ export function CollapsibleTable() {
   );
 }
 
-const store = new Store("ett-settings", "settings", {
+const store = new Store("ett-functions", "functions", {
   keyPath: "type",
 });
 const FnStore = () => {
+  const [excelFunctions, setExcelFunctions] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  useEffect(() => {
+    (async () => {
+      const newExcelFunctions = [
+        createData("Frozen yoghurt", 159, 6.0, 24),
+        createData("Frozen yoghurt", 159, 6.0, 24),
+        createData("Frozen yoghurt", 159, 6.0, 24),
+        createData("Frozen yoghurt", 159, 6.0, 24),
+        createData("Frozen yoghurt", 159, 6.0, 24),
+        createData("Frozen yoghurt", 159, 6.0, 24),
+        createData("Frozen yoghurt", 159, 6.0, 24),
+      ];
+      setExcelFunctions(newExcelFunctions);
+    })();
+  }, []);
   return (
     <div className="FnStore">
-      <CollapsibleTable />
+      <CollapsibleTable excelFunctions={excelFunctions} />
+      <div
+        className={"add-function"}
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <IconButton
+          style={{
+            display: "flex",
+          }}
+        >
+          <AddCircleIcon />
+        </IconButton>
+      </div>
     </div>
   );
 };
